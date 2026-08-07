293caa74544c5ff3944366c26c7bc09d3b2d3b14
Long <lphlong@baominh.com.vn>
2026-08-07 16:58:35 +0700

    [Add] Add send message logic

--- ChatApp.Api/Hubs/ChatHub.cs
+++ ChatApp.Api/Hubs/ChatHub.cs
@@ -5,6 +5,7 @@ using ChatApp.Api.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace ChatApp.Api.Hubs
 {
@@ -177,6 +178,76 @@ namespace ChatApp.Api.Hubs
             }
         }
 
+        public async Task SendMessage(SendMessageRequestDto request)
+        {
+            var connectionId = Context.ConnectionId;
+
+            try
+            {
+                var userId = Context.GetUserId();
+
+                await EnsureMembership(userId, request.RoomId);
+
+                var user = await _context.Users.FindAsync(userId);
+
+                if (user == null)
+                {
+                    throw new HubException("Người dùng không tồn tại trong hệ thống.");
+                }
+
+                await JoinRoom(request.RoomId);
+
+                var message = new Message
+                {
+                    Id = Guid.NewGuid(),
+                    RoomId = request.RoomId,
+                    SenderId = user.Id,
+                    Content = request.Content,
+                    ReplyToMessageId = request.ReplyToMessageId,
+                    Type = request.MessageType,
+                    SentAt = DateTime.UtcNow
+                };
+
+                _context.Messages.Add(message);
+                await _context.SaveChangesAsync();
+
+                // Gửi tin nhắn đến tất cả các thành viên trong phòng chat
+                await Clients.Group($"room_{request.RoomId}").SendAsync("ReceiveMessage", new MessageDto { 
+                    Id = message.Id,
+                    RoomId = message.RoomId,
+                    MessageType = message.Type,
+                    Content = message.Content,
+                    ReplyToMessageId = message.ReplyToMessageId,
+                    SentAt = message.SentAt,
+                    Sender = new SenderDto
+                    {
+                        Id = message.SenderId,
+                        Username = user.Username,
+                        AvatarUrl = user.AvatarUrl
+                    }
+                });
+
+            }
+            catch (Exception ex)
+            {
+                // Ghi log lỗi nếu quá trình dọn dẹp database thất bại
+                _logger.LogError(ex, "Không thể gửi tin nhắn: {ConnectionId}", connectionId);
+            }
+        }
+
+        public async Task JoinRoom(Guid roomId)
+        {
+            var userId = Context.GetUserId();
+            await EnsureMembership(userId, roomId);
+            bool existedRoom = await _context.Rooms.AnyAsync(r => r.Id == roomId);
+            if (!existedRoom)
+            {
+                throw new HubException("Phòng chat không tồn tại.");
+            }
+            // Thêm ConnectionId vào Group của phòng chat
+            await Groups.AddToGroupAsync(Context.ConnectionId, $"room_{roomId}");
+        }
+
         private async Task<List<Guid>> GetRelatedUserId(Guid? userId)
         {
             var friendIds = _context.Friendships
@@ -194,24 +265,7 @@ namespace ChatApp.Api.Hubs
             return allContacts;
         }
 
-        public async Task SendMessage(SendMessageRequestDto request)
-        {
-            var userId = Context.GetUserId();
-
-            // Kiểm tra an toàn trước khi Parse Guid để tránh văng Exception không đáng có
-            if (userId == null)
-            {
-                // Trả lỗi định dạng rõ ràng về client
-                await Clients.Caller.SendAsync("OnConnectionError", "Thông tin định danh người dùng không hợp lệ hoặc thiếu.");
-                Context.Abort(); // Ngắt kết nối ngay lập tức
-                return;
-            }
-
-            await EnsureMembership(userId.Value, request.RoomId);
-
-        }
-
-        private async Task EnsureMembership(Guid userId, Guid roomId)
+        private async Task EnsureMembership(Guid? userId, Guid roomId)
         {
             var isMember = await _context.RoomMembers
                 .AnyAsync(rm => rm.RoomId == roomId && rm.UserId == userId);
